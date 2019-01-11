fd9a80e9ef6fe6a929186c17557735534d0d0e18
Mvcampbell3 <mvcampbell3@gmail.com>
2019-01-11 15:14:39 -0500

    continuing to work on auth in js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,33 +6,34 @@ var config = {
     projectId: "rockpaperscissors-193da",
     storageBucket: "rockpaperscissors-193da.appspot.com",
     messagingSenderId: "72589420438"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var auth = firebase.auth();
+var auth = firebase.auth();
 //   Vars that decide which player you are
 var justSigned = false;
+var displayName;
 
 //   Btns that control showing the correct login pages and their functions
-$(".loginGame").on("click", function(){
+$(".loginGame").on("click", function () {
     $(".login").fadeIn();
     console.log($(this).attr("data-player"));
     console.log($(this).attr("data-rpsClass"));
     console.log($(this).attr("data-butArea"));
-    $("body").css({"background-color":"#333"});
+    $("body").css({ "background-color": "#333" });
 });
 
-$(".backBtn").on("click", function(){
-    $("body").css({"background-color": "#fff"});
+$(".backBtn").on("click", function () {
+    $("body").css({ "background-color": "#fff" });
     $(".login").fadeOut();
 });
 
-$(".signUpBackBtn").on("click", function(){
+$(".signUpBackBtn").on("click", function () {
     $(".signUp").fadeOut();
     $(".login").fadeIn();
 });
 
-$(".toSignUpBtn").on("click", function(){
+$(".toSignUpBtn").on("click", function () {
     $(".login").fadeOut();
     $(".signUp").fadeIn();
 })
@@ -40,12 +41,55 @@ $(".toSignUpBtn").on("click", function(){
 
 // Authentication stuff
 
-$(".signUpBtn").on("click", function(){
-    var email = $(".signUpEmail").val().trim();
-    var displayName = $(".signUpDisplayName").val().trim();
-    var password = $(".signUpPassword").val().trim();
+$(".signUpBtn").on("click", function () {
+    var email = $("#signUpEmail").val().trim();
+    var password = $("#signUpPassword").val().trim();
+    displayName = $("#signUpDisplayName").val().trim();
 
-    if (email === "" || displayName === "" || password === ""){
+    if (email === "" || displayName === "" || password === "") {
         alert("Must Enter Information In All Fields");
+    } else {
+        var promise = auth.createUserWithEmailAndPassword(email, password);
+        promise.catch(function (event) {
+            console.log(event.message);
+        });
+        justSigned = true;
+        $(".signUp").fadeOut();
+        $("body").css({ "background-color": "#fff" });
+
+    }
+})
+
+$(".loginBtn").on("click", function () {
+    console.log("loginBtn clicked");
+    var email = $("#loginEmail").val().trim();
+    var password = $("#loginPassword").val().trim();
+
+    var promise = auth.signInWithEmailAndPassword(email, password);
+    promise.catch(function (event) {
+        console.log(event.message);
+    });
+
+    $(".login").fadeOut();
+    $("body").css({ "background-color": "#fff" });
+});
+
+// Logout Btns
+$(".logoutBtn").on("click", function(){
+    firebase.auth().signOut();
+})
+
+firebase.auth().onAuthStateChanged(function(user){
+    if (user && justSigned) {
+        justSigned = false;
+        user.updateProfile({
+            displayName: displayName,
+        });
+        console.log(user);
+    } else if (user && !justSigned) {
+        console.log("logged in again");
+        console.log(user);
+    } else {
+        console.log("not logged in");
     }
 })
